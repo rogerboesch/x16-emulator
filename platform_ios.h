764bdb6270838f90f74aeebdc4678bb5535f2cb2
Roger <roger@captural..com>
2021-02-19 13:30:46 +0100

    file load/save support

--- platform_ios.h
+++ platform_ios.h
@@ -8,13 +8,21 @@
 
 #include <stdio.h>
 
+// Path support
 char* platform_get_base_path(void);
 char* platform_get_gif_path(void);
 double platform_get_ticks(void);
 
+// File support
 FILE* platform_file_open(char* path, char* mode);
+void platform_file_close(FILE* fp);
 size_t platform_file_read(FILE* fp, void* buf, size_t size, size_t mnemb);
+uint8_t platform_file_read_u8(FILE* fp);
+size_t platform_file_write(FILE* fp, void* buf, size_t size, size_t mnemb);
+void platform_file_write_u8(FILE* fp, uint8_t byte);
+int platform_file_seek(FILE *stream, long int offset, int whence);
 
+// Render support
 void platform_render_buffer(uint8_t* framebuffer);
 
 // Temporary, replace later
@@ -26,19 +34,18 @@ void platform_render_buffer(uint8_t* framebuffer);
 #define SDL_Surface void
 #define SDL_Texture void
 #define SDL_Window void
+#define SDL_Renderer void
+#define SDL_GameController void
+#define SDL_GetTicks void
 
 #define SDL_RWops FILE
 #define SDL_RWread platform_file_read
-#define SDL_RWseek fseek
-#define SDL_RWwrite fwrite
+#define SDL_RWseek platform_file_seek
+#define SDL_RWwrite platform_file_write
 #define SDL_RWFromFile platform_file_open
-#define SDL_RWclose fclose
-#define SDL_Renderer void
-#define SDL_GameController void
-
-#define SDL_GetTicks void
-#define SDL_ReadU8
-#define SDL_WriteU8
+#define SDL_RWclose platform_file_close
+#define SDL_ReadU8 platform_file_read_u8
+#define SDL_WriteU8 platform_file_write_u8
 
 #endif
 #endif /* platform_ios_h */
--- xcode/x16-ios/Platform.m
+++ xcode/x16-ios/Platform.m
@@ -19,6 +19,12 @@ char* platform_get_base_path(void) {
     return (char *)[path UTF8String];
 }
 
+char* platform_get_documents_path(void) {
+    NSArray* paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory,NSUserDomainMask, YES);
+    NSString* documentsDirectory = [paths objectAtIndex:0];
+    return (char *)[documentsDirectory UTF8String];
+}
+
 char* platform_get_gif_path(void) {
     NSString* path = [[NSURL fileURLWithPath:NSHomeDirectory()] path];
     path = [NSString stringWithFormat:@"%@/record.gif", path];
@@ -34,9 +40,39 @@ double platform_get_ticks(void) {
 }
 
 FILE* platform_file_open(char* path, char* mode) {
+    if (path[0] != '/') {
+        // Load/save is just possible in document directory
+        char fullPath[256];
+        sprintf(fullPath, "%s/%s", platform_get_documents_path(), path);
+    
+        return fopen(fullPath, mode);
+    }
+    
     return fopen(path, mode);
+}
+
+void platform_file_close(FILE* fp) {
+    fclose(fp);
 }
 
 size_t platform_file_read(FILE* fp, void* buf, size_t size, size_t mnemb) {
     return fread(buf, size, mnemb, fp);
+}
+
+size_t platform_file_write(FILE* fp, void* buf, size_t size, size_t mnemb) {
+    return fwrite(buf, size, mnemb, fp);
+}
+
+uint8_t platform_file_read_u8(FILE* fp) {
+    uint8_t buffer;
+    fread(&buffer, 1, 1, fp);
+    return buffer;
+}
+
+void platform_file_write_u8(FILE* fp, uint8_t byte) {
+    fwrite (&byte, 1, 1, fp);
+}
+
+int platform_file_seek(FILE *fp, long int offset, int whence) {
+    return fseek(fp, offset, whence);
 }
